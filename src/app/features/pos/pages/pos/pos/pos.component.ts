f0c4b7d973141262cd5a90733e069148b17be349
RosalesDev <119462488+RosalesDev@users.noreply.github.com>
2024-12-10 00:37:15 -0300

    Probando cursor. Se agrega funcion para restar y eliminar un producto agregado a la lista

--- src/app/features/pos/pages/pos/pos/pos.component.ts
+++ src/app/features/pos/pages/pos/pos/pos.component.ts
@@ -4,12 +4,32 @@ import { CurrencyPipe } from '@angular/common';
 import { Product } from '../../../../../core/models/product-model';
 import { AuthService } from '../../../../../core/services/auth.service';
 import { Router } from '@angular/router';
+import { trigger, style, animate, transition } from '@angular/animations';
 
 @Component({
   selector: 'app-pos',
   imports: [CurrencyPipe],
   templateUrl: './pos.component.html',
   styleUrl: './pos.component.css',
+  animations: [
+    trigger('fadeOutAnimation', [
+      transition(':leave', [
+        animate(
+          '300ms ease-out',
+          style({
+            opacity: 0,
+            transform: 'translateX(-100%)',
+          })
+        ),
+      ]),
+    ]),
+    trigger('quantityAnimation', [
+      transition('* => *', [
+        style({ scale: 1.3, color: '#4CAF50' }),
+        animate('300ms ease-out', style({ scale: 1, color: '*' })),
+      ]),
+    ]),
+  ],
 })
 export class PosComponent {
   scannedProducts = signal<Product[]>([]); // Lista de productos escaneados
@@ -58,6 +78,24 @@ export class PosComponent {
   clearCart(): void {
     this.scannedProducts.set([]); // Limpia los productos
     this.subtotal.set(0); // Reinicia el subtotal
+  }
+
+  removeProduct(productId: string): void {
+    this.scannedProducts.update((products) =>
+      products
+        .map((product) => {
+          if (product.id === productId) {
+            // Si la cantidad es mayor a 1, reducimos en 1
+            if (product.quantity > 1) {
+              return { ...product, quantity: product.quantity - 1 };
+            }
+            // Si la cantidad es 1, no incluimos el producto (lo eliminamos)
+            return null;
+          }
+          return product;
+        })
+        .filter((product): product is Product => product !== null)
+    );
   }
 
   logOut() {
