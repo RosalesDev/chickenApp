35a7ed30feaab106430753734ac2e850e583f026
RosalesDev <119462488+RosalesDev@users.noreply.github.com>
2025-08-02 00:22:39 -0300

    cambios para switchear entre ambientes

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firebaseConfig: {
+    apiKey: 'AIzaSyDDEBjRpo1JOVt5cUytPEs_0OgJslqfaPc',
+    authDomain: 'control-ventas-39e9a.firebaseapp.com',
+    projectId: 'control-ventas-39e9a',
+    storageBucket: 'control-ventas-39e9a.firebasestorage.app',
+    messagingSenderId: '598267961127',
+    appId: '1:598267961127:web:057ad5d2d715133ace9302',
+  },
+};
--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { AppComponent } from './app/app.component';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment.development';
+import { environment } from './environments/environment';
 import { initializeApp } from 'firebase/app';
 
 if (environment.production) {
